09ee5c6593e052e32ee082d0fd5b8118639429dc
Jose Miguel Perez <jose.perez@mapfre.cl>
2025-08-06 11:49:00 -0400

    feat: NavigationSidebar for admin pages

--- app/components/admin/NavigationSidebar.tsx
+++ app/components/admin/NavigationSidebar.tsx
@@ -1,5 +1,32 @@
-export default function NavigationSidebar() {
-    return <aside className="fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 border-r-gray-400 text-white h-screen z-50 border-r">
+import Link from "next/link";
+import Logo from "../../ui/shared/logo";
+import { IconHome } from "@tabler/icons-react";
 
+export default function NavigationSidebar() {
+    return <aside className="fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 border-r-gray-400 text-white h-screen z-50 border-r w-[290px] -translate-x-full lg:translate-x-0">
+        <div className="py-8 flex justify-start">
+            <Logo />
+        </div>
+        <div className="flex flex-col overflow-y-auto">
+            <nav className="mb-6">
+                <div className="flex flex-col gap-4">
+                    <div>
+                        <h2 className="mb-4 text-xs uppercase flex leading-[20px] text-gray-400 justify-start">Menu</h2>
+                        <ul className="flex flex-col gap-4">
+                            <li>
+                                <Link href="/" className="menu-item group menu-item-inactive">
+                                    <span className="menu-item-icon-inactive">
+                                        <IconHome />
+                                    </span>
+                                    <span className="menu-item-text">
+                                        Inicio
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </aside>
 }
